773746d837d1c370db7f4716e7915d2f8507bccd
Tiago Flor <tiagoxxsilva@hotmail.com>
2022-08-16 17:55:45 -0300

    commit do cypress contendo  pre cadastro

--- cypress/e2e/pre_cadastro.cy.js
+++ cypress/e2e/pre_cadastro.cy.js
@@ -8,15 +8,21 @@ describe('Deve completar o pré cadastro', () => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
     });
 
+    afterEach(() => {
+        cy.screenshot()
+    });
+
     it('Deve complettar o pré cadastro com sucesso', () => {
-        cy.get('#reg_email').type(faker.internet.email()) // chamando a variavel faker pra usar os metodos
+        cy.get('#reg_email').type(faker.internet.email()) // chamando a metodo faker para criar email aleatorio
         cy.get('#reg_password').type('!teste@teste2')
         cy.get(':nth-child(4) > .button').click()
         
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type('Tiago')
-        cy.get('#account_last_name').type('Silva')
+        cy.get('#account_first_name').type(faker.name.firstName()) // chamando a metodo faker para criar nome
+        cy.get('#account_last_name').type(faker.name.lastName()) // chamando a metodo faker para criar ultimo nome
         cy.get('.woocommerce-Button').click()
+
+        cy.get('.woocommerce-message').should('contain' , 'Detalhe da conta modificados com sucesso')
 
     });
 
