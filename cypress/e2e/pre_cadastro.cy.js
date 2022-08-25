58c47398b205a42726c20eee60ec6d51e5df70fd
Tiago Flor <tiagoxxsilva@hotmail.com>
2022-08-25 09:46:27 -0300

    commit aula listas e variáveis

--- cypress/e2e/pre_cadastro.cy.js
+++ cypress/e2e/pre_cadastro.cy.js
@@ -27,3 +27,40 @@ describe('Deve completar o pré cadastro', () => {
     });
 
 });
+
+/*
+ OUTRA FORMA DE FAZER
+ --------------------
+ 
+const { faker } = require('@faker-js/faker'); //usado para criar email fake dinamico
+
+describe('Deve completar o pré cadastro', () => {
+    var nomeFaker = faker.name.firstName()
+    var sobreNomeFaker = faker.name.lastName()
+    var emailFaker = faker.internet.email()
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Deve complettar o pré cadastro com sucesso', () => {
+        cy.get('#reg_email').type(emailFaker) // chamando a metodo faker para criar email aleatorio
+        cy.get('#reg_password').type('!teste@teste2')
+        cy.get(':nth-child(4) > .button').click()
+        
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(nomeFaker) // chamando a metodo faker para criar nome
+        cy.get('#account_last_name').type(sobreNomeFaker) // chamando a metodo faker para criar ultimo nome
+        cy.get('.woocommerce-Button').click()
+
+        cy.get('.woocommerce-message').should('contain' , 'Detalhe da conta modificados com sucesso')
+
+    });
+
+});
+
+*/ 
---
+++ cypress/e2e/produtos.cy.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade Página de produtos', () => {
+    before(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos')
+    });
+
+
+    it('Deve seleciona um produto da lista', () => {
+        cy.get('[class="product-block grid"]') //utilizando dessa forma você seleciona a classe que quer usar
+        //.first()   //metodo que seleciona o primeiro da lista
+        //.last() // metodo que seleciona o ultimo da lista
+        //.eq(2) // metodo que seleciona a posição de um item da lista
+        .contains('Ariel Roll Sleeve Sweatshirt').click()  //seleciona uma palavra chave (caso o elemento seja clicavel)
+    });
+
+    it.only('Deve adicionar um produto ao carrinho', () => {
+        var quantidade = 15  //declarando uma váriavel é só passar os parâmetros conforme exemplo abaixo
+
+        cy.get('[class="product-block grid"]')
+            .contains('Ajax Full-Zip Sweatshirt').click()
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Green').click()
+        //cy.get('.input-text').clear().type(2) //limpa o campo e adiciona 2 no carrinho
+        cy.get('.input-text').clear().type(quantidade) //passou parâmetro quantidade ao invés de trocar tudo é só passar o parâmetro
+        cy.get('.single_add_to_cart_button').click()
+
+        //exemplo resultado 1
+        //cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 2)
+        //exemplo resultado 2
+        //cy.get('.woocommerce-message').should('contains', '2 × “Ajax Full-Zip Sweatshirt” foram adicionados no seu carrinho.')
+
+        //exemplo resultado com váriaveis
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        //exemplo resultado com varíaveis
+        cy.get('.woocommerce-message').should('assert', quantidade + ' x “Ajax Full-Zip Sweatshirt” foram adicionados no seu carrinho.')
+
+    });
+    
+});
