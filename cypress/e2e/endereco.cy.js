58ed7f9e548aeac7a9ae4ab25d6297fc597427d4
Tiago Flor <tiagoxxsilva@hotmail.com>
2022-10-26 10:30:01 -0300

    aula page object parte 1

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,3 +1,5 @@
+import EnderecoPage from '../support/page-objects/endereco.page' //importando Page Objects
+
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
      beforeEach(() => {
           cy.visit('minha-conta')
@@ -7,9 +9,12 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
           })
      });
 
-
-     it('Deve fazer cadastro de faturamento com sucesso', () => {
-          //cadastro de endereço
-
+     // cenário utilizando Page Object
+     it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+          //cadastro de endereço passando os parametros
+          EnderecoPage.editarEnderecoFaturamento('Tiago', 'Flor', 'EBAC', 'Brasil', 'Rua Adail', '222','Rio de Janeiro', 'Rio de Janeiro', '21040000', '21999999999', 'teste@teste.com.br' )
+          cy.get('.woocommerce-message').should('contain','Endereço alterado com sucesso.')
      });
+
+
 });
---
+++ cypress/support/page-objects/endereco.page.js
@@ -0,0 +1,46 @@
+// criado PAGE OBJECTS
+class EnderecoPage {
+     // Passar os parametros dos metodos e chama-los no  step
+     editarEnderecoFaturamento(nome, sobrenome, empresa, pais, endereco, numero, cidade, estado, cep, telefone, email) {
+          //elementos + acoes
+          cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
+          cy.get(':nth-child(1) > .title > .edit').click()
+          cy.get('#billing_first_name').clear().type(nome) //usar o clear para limpar
+          cy.get('#billing_last_name').clear().type(sobrenome)
+          cy.get('#billing_company').clear().type(empresa)
+          cy.get('#select2-billing_country-container').click().type(pais).get('[aria-selected="true"]').click()
+          cy.get('#billing_address_1').clear().type(endereco)
+          cy.get('#billing_address_2').clear().type(numero)
+          cy.get('#billing_city').clear().type(cidade)
+          //exemplo utilizando ações do teclado e concatenando
+          cy.get('#select2-billing_state-container').click().type(estado + '{enter}')
+          cy.get('#billing_postcode').clear().type(cep)
+          cy.get('#billing_phone').clear().type(telefone)
+          cy.get('#billing_email').clear().type(email)
+          cy.get('.button').click()
+
+          /*     Exemplo Page object inicial sem passar os parametros
+          cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
+          cy.get(':nth-child(1) > .title > .edit').click()
+          cy.get('#billing_first_name').clear().type('Tiago') //usar o clear para limpar
+          cy.get('#billing_last_name').clear().type('Silva')
+          cy.get('#billing_company').clear().type('EBAC')
+          cy.get('#select2-billing_country-container').click().type('Brasil').get('[aria-selected="true"]').click()
+          cy.get('#billing_address_1').clear().type('Rua Adail')
+          cy.get('#billing_address_2').clear().type('222')
+          cy.get('#billing_city').clear().type('Rio de Janeiro')
+          cy.get('#select2-billing_state-container').click().type('Rio de Janeiro{enter}') //exemplo utilizando ações do teclado
+          cy.get('#billing_postcode').clear().type('21040000')
+          cy.get('#billing_phone').clear().type('21999999999')
+          cy.get('#billing_email').clear().type('teste@teste.com.br')
+          cy.get('.button').click()
+          */
+     }
+
+     editarEnderecoEntrega() {
+          //elementos + acoes
+     }
+
+}
+
+export default new EnderecoPage() //permite que seja importado em outros arquivos de teste
